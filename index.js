e7cb0ec7a78d56cff8fdb4f61ebfcb77b93bec29
Andrii Biakov <an.bjakov@gmail.com>
2022-09-18 00:09:33 +0300

    several changes

--- index.js
+++ index.js
@@ -1,50 +1,59 @@
 'use strict'
 
 const taskManageForm = document.forms.tasks;
+
 taskManageForm.onsubmit = (event)=>{
     event.preventDefault();
-}
-const {taskDescription} = taskManageForm;
-const taskList = document.getElementById('list');
-
-taskList.onclick = deleteTask;
-taskManageForm.onclick = submitTask;
-
-function deleteTask (event){
-    const targetTask = event.target;
-    if (targetTask.name  === 'deleteTask')  {
-        targetTask.closest('li').classList.toggle("animate__fadeOutLeft");
-        targetTask.closest('li').addEventListener('animationend', () =>{
-            targetTask.closest('li').remove();
-        })
-
-    }
-}
-taskDescription.oninput = ()=>{
-    taskDescription.classList.remove("danger")
-}
-
-function submitTask (event){
-    const node = event.target;
-    if (node.name  === 'submitTask'){
         if (taskDescription.value.trim()==="") {
-            taskDescription.classList.add("danger","animate__animated","animate__shakeX");
+            taskDescription.classList.add("border-danger","animate__animated","animate__shakeX");
             taskDescription.addEventListener('animationend', () => {
                 taskDescription.classList.remove("animate__animated", "animate__shakeX")
             });
         }
         else{
-            const task = document.createElement('li');
-            const deleteButton = document.createElement('button');
-            deleteButton.name="deleteTask";
-            deleteButton.innerHTML = "Delete";
-            task.innerHTML = taskDescription.value;
-            task.classList.add("animate__animated","animate__fadeInLeft")
-            task.append(deleteButton);
-            taskList.append(task);
+            taskList.append(addTask(taskDescription.value));
             taskDescription.value = "";
         }
+}
+const {taskDescription} = taskManageForm;
+const taskList = document.getElementById('list');
+
+taskList.onclick = deleteTask;
+function addTask(taskContent) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    const contentCol = document.createElement('div');
+    contentCol.classList.add('col-10');
+    const manageCol = document.createElement('div');
+    manageCol.classList.add('col-2');
+    const task = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    deleteButton.name="deleteTask";
+    deleteButton.innerHTML = '<i class=\"bi bi-trash pointerEvents\"></i> Delete';
+    deleteButton.classList.add('btn', 'btn-primary',"w-100","h-100","border")
+    manageCol.append(deleteButton);
+    task.innerHTML = taskContent;
+    task.classList.add("list-group-item","animate__animated","animate__fadeIn");
+    deleteButton.classList.add("animate__animated","animate__fadeIn")
+
+    contentCol.append(task);
+    row.append(contentCol,manageCol)
+    return row
+}
+function deleteTask (event){
+    const targetTask = event.target;
+    console.log(event.target.closest('.row'))
+    if (targetTask.name  === 'deleteTask')  {
+        targetTask.closest('.row').classList.toggle("animate__fadeOut");
+        targetTask.closest('.row').addEventListener('animationend', () =>{
+            targetTask.closest('.row').remove();
+
+        })
 
     }
 }
+taskDescription.oninput = ()=>{
+    taskDescription.classList.remove("border-danger");
+}
+
 
